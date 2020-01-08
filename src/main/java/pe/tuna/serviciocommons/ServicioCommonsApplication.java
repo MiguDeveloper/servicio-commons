7cb4e6722fa8e5710749fc62a6e0b5af5d50e9d9
MiguDeveloper <miguel.peru.seo@gmail.com>
2020-01-08 16:34:05 -0500

    inicio commons lib

---
+++ src/main/java/pe/tuna/serviciocommons/ServicioCommonsApplication.java
@@ -0,0 +1,15 @@
+package pe.tuna.serviciocommons;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioCommonsApplication {
+
+    // Al tratarse de un proyecto de libreria no necesitamos que se ejecute por lo tanto no necesitamos el metodo Main()
+    /*
+    public static void main(String[] args) {
+        SpringApplication.run(ServicioCommonsApplication.class, args);
+    }
+    */
+
+}
